530bdcf893a1ac0c00c810d582019fe052f8853e
subb1n <haphpy@naver.com>
2024-03-19 04:01:11 +0900

    카카오 맵 api로 현재 위치 설정까지 했습니닷

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,11 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(files("libs/libDaumMapAndroid.jar"))
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("com.google.android.gms:play-services-location:18.0.0")
 }
