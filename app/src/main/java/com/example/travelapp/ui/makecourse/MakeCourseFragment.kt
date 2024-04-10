adf7a336948dd60911fcf372ca75b38d94e440c8
subb1n <haphpy@naver.com>
2024-04-10 17:19:56 +0900

    test

--- app/src/main/java/com/example/travelapp/ui/makecourse/MakeCourseFragment.kt
+++ app/src/main/java/com/example/travelapp/ui/makecourse/MakeCourseFragment.kt
@@ -28,10 +28,10 @@ class MakeCourseFragment : Fragment() {
         _binding = FragmentMakecourseBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+        /*val textView: TextView = binding.textNotifications
         makeCourseViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
         return root
     }
 
